e1aafd00c76f7ac46e1734e219596a4b96891265
andrelrv <118623026+andrelrv@users.noreply.github.com>
2022-11-26 19:12:19 -0300

    Função que atualiza a matriz
    É uma matriz que atualiza a atual com base nos valores armazenados na antiga. 
    
    O meu objetivo principal na hora de fazer essa função foi separar a leitura dos cantos e da borda, da leitura do miolo da matriz, visto que se eu deixasse todas no mesmo laço o programa teria que fazer mais umas 1500 comparações toda vez q ele fosse analisar um novo termo.
    
    Mesmo assim acabou ficando beeeeem maior doque eu gostaria, eu tentei até dar uma enxutada em algumas partes, mas o problema era que se eu acabasse encolhendo demais o código, o programa teria que fazer muito mais comparações doque essa versão. Em questão de encolher, consegui juntar as comparações dos 4 cantos e 4 bordas em pares graças a uma variável chamada "coordenada_fixa".
    
    Provavelmente deve haver mais alguma maneira de encolher o código sem dobrar o tempo de processamento dele, no entanto não há muito como fugir de um código gigante na minha opinião.

--- src/uteis.c
+++ src/uteis.c
@@ -113,3 +113,248 @@ char **split(char *str, char *separador, int *contPalavras)
 
   return palavras;
 }
+
+/**
+* Atualiza a matriz atual com base nos dados da matriz anterior
+*
+* A matriz anterior funciona é imutável e funciona como base para as comparações
+*
+* @param cicloAnt matriz anterior
+* @param cicloAtual matriz que será atualizada
+* @param nl número de linhas
+* @param nc número de colunas
+*/
+void atualizaMat(char **cicloAnt, char **cicloAtual, int nl, int nc)
+{
+    int i, j, coordenada_fixa =0, vizinhas =0;
+
+    //*==Lendo cada canto da matriz==*
+
+    //canto superior e inferior esquerdo:
+    for(i=0; i<2; i++)
+    {
+        if(i == 0)//canto superior
+        {
+        if(cicloAnt[0][1] == 'ORG')
+            vizinhas++;
+        if(cicloAnt[1][1] == 'ORG')
+            vizinhas++;
+        if(cicloAnt[1][0] == 'ORG')
+            vizinhas++;
+        }
+        else//canto inferior
+        {
+        if(cicloAnt[nl-1][1] == 'ORG')
+            vizinhas++;
+        if(cicloAnt[nl-2][1] == 'ORG')
+            vizinhas++;
+        if(cicloAnt[nl-2][0] == 'ORG')
+            vizinhas++;
+        }
+
+
+        if(cicloAnt[coordenada_fixa][0] == 'VAZ' && vizinhas == 3)
+            cicloAtual[coordenada_fixa][0] = 'ORG';
+        else if(cicloAnt[coordenada_fixa][0] == 'ORG' && vizinhas < 2)
+            cicloAtual[coordenada_fixa][0] = 'VAZ';
+
+        vizinhas = 0;//resetando contador
+
+        coordenada_fixa+=nl-1; //aumenta a coordenada da linha para nl-1 (última linha)
+
+    }
+
+    coordenada_fixa = 0;//resetando para zero
+
+    //canto superior e inferior direito:
+    for(i=0; i<2; i++)
+    {
+        if(i == 0)//canto superior
+        {
+        if(cicloAnt[0][nc-2] == 'ORG')
+            vizinhas++;
+        if(cicloAnt[1][nc-2] == 'ORG')
+            vizinhas++;
+        if(cicloAnt[1][nc-1] == 'ORG')
+            vizinhas++;
+        }
+        else//canto inferior
+        {
+        if(cicloAnt[nl-1][nc-2] == 'ORG')
+            vizinhas++;
+        if(cicloAnt[nl-2][nc-2] == 'ORG')
+            vizinhas++;
+        if(cicloAnt[nl-2][nc-1] == 'ORG')
+            vizinhas++;
+        }
+
+
+        if(cicloAnt[coordenada_fixa][nc-1] == 'VAZ' && vizinhas == 3)
+            cicloAtual[coordenada_fixa][nc-1] = 'ORG';
+        else if(cicloAnt[coordenada_fixa][nc-1] == 'ORG' && vizinhas < 2)
+            cicloAtual[coordenada_fixa][nc-1] = 'VAZ';
+
+        vizinhas = 0;//resetando contador
+
+        coordenada_fixa+=nl-1; //aumenta a coordenada da linha para nl-1 (última linha)
+
+    }
+
+    coordenada_fixa = 0;//resetando para zero
+
+    //*==Lendo as bordas (sem os cantos)==*
+
+    //bordas horizontais:
+    for(i=0; i<2; i++)
+    {
+        if(i == 0)//borda superior
+            for(j=1; j<nc-1; j++)//vai da segunda posição até a penúltima posição da linha
+            {
+                if(cicloAnt[coordenada_fixa][j-1] == 'ORG')
+                    vizinhas++;
+                if(cicloAnt[coordenada_fixa+1][j-1] == 'ORG')
+                    vizinhas++;
+                if(cicloAnt[coordenada_fixa+1][j] == 'ORG')
+                    vizinhas++;
+                if(cicloAnt[coordenada_fixa+1][j+1] == 'ORG')
+                    vizinhas++;
+                if(cicloAnt[coordenada_fixa][j+1] == 'ORG')
+                    vizinhas++;
+
+                if(cicloAnt[coordenada_fixa][j] == 'VAZ' && vizinhas == 3)
+                    cicloAtual[coordenada_fixa][j] = 'ORG';
+                else if((cicloAnt[coordenada_fixa][j] == 'ORG' && vizinhas < 2) || (cicloAnt[coordenada_fixa][j] == 'ORG' && vizinhas > 3))
+                    cicloAtual[coordenada_fixa][j] = 'VAZ';
+
+                vizinhas = 0;//resetando contador
+            }
+        else //borda inferior
+            for(j=1; j<nc-1; j++)
+            {
+                if(cicloAnt[coordenada_fixa][j-1] == 'ORG')
+                    vizinhas++;
+                if(cicloAnt[coordenada_fixa-1][j-1] == 'ORG')
+                    vizinhas++;
+                if(cicloAnt[coordenada_fixa-1][j] == 'ORG')
+                    vizinhas++;
+                if(cicloAnt[coordenada_fixa-1][j+1] == 'ORG')
+                    vizinhas++;
+                if(cicloAnt[coordenada_fixa][j+1] == 'ORG')
+                    vizinhas++;
+
+                if(cicloAnt[coordenada_fixa][j] == 'VAZ' && vizinhas == 3)
+                    cicloAtual[coordenada_fixa][j] = 'ORG';
+                else if((cicloAnt[coordenada_fixa][j] == 'ORG' && vizinhas < 2) || (cicloAnt[coordenada_fixa][j] == 'ORG' && vizinhas > 3))
+                    cicloAtual[coordenada_fixa][j] = 'VAZ';
+
+                vizinhas = 0;//resetando contador
+            }
+
+        coordenada_fixa+=nl-1; //aumenta a coordenada a linha em nl-1 (última linha)
+
+    }
+
+    coordenada_fixa = 0;
+
+    //bordas verticais:
+    for(i=0; i<2; i++)
+    {
+        if(i == 0) //borda da esquerda
+            for(j=1; j<nl-1; j++) // vai da segunda posição até a penúltima posição da coluna
+            {
+                if(cicloAnt[j-1][coordenada_fixa] == 'ORG')
+                    vizinhas++;
+                if(cicloAnt[j-1][coordenada_fixa+1] == 'ORG')
+                    vizinhas++;
+                if(cicloAnt[j][coordenada_fixa+1] == 'ORG')
+                    vizinhas++;
+                if(cicloAnt[j+1][coordenada_fixa+1] == 'ORG')
+                    vizinhas++;
+                if(cicloAnt[j+1][coordenada_fixa] == 'ORG')
+                    vizinhas++;
+
+                if(cicloAnt[j][coordenada_fixa] == 'VAZ' && vizinhas == 3)
+                    cicloAtual[j][coordenada_fixa] = 'ORG';
+                else if((cicloAnt[j][coordenada_fixa] == 'ORG' && vizinhas < 2) || (cicloAnt[coordenada_fixa][j] == 'ORG' && vizinhas > 3))
+                    cicloAtual[j][coordenada_fixa] = 'VAZ';
+
+                vizinhas = 0;//resetando contador
+            }
+        else //borda da direita
+            for(j=1; j<nl-1; j++)
+            {
+                if(cicloAnt[j-1][coordenada_fixa] == 'ORG')
+                    vizinhas++;
+                if(cicloAnt[j-1][coordenada_fixa-1] == 'ORG')
+                    vizinhas++;
+                if(cicloAnt[j][coordenada_fixa-1] == 'ORG')
+                    vizinhas++;
+                if(cicloAnt[j+1][coordenada_fixa-1] == 'ORG')
+                    vizinhas++;
+                if(cicloAnt[j+1][coordenada_fixa] == 'ORG')
+                    vizinhas++;
+
+                if(cicloAnt[j][coordenada_fixa] == 'VAZ' && vizinhas == 3)
+                    cicloAtual[j][coordenada_fixa] = 'ORG';
+                else if((cicloAnt[j][coordenada_fixa] == 'ORG' && vizinhas < 2) || (cicloAnt[j][coordenada_fixa] == 'ORG' && vizinhas > 3))
+                    cicloAtual[j][coordenada_fixa] = 'VAZ';
+
+                vizinhas = 0;//resetando contador
+            }
+
+         coordenada_fixa += nc-1; //aumenta a coordenada da coluna em nc-1 (última coluna)
+
+    }
+
+    //*==Lendo o miolo==*
+
+    for(i=1; i<nl-1; i++)//vai da segunda a penúltima linha
+    {
+        for(j=1; j<nc-1; j++)//vai da segunda a penúltima coluna
+        {
+            if(cicloAnt[i-1][j-1] == 'ORG')
+                vizinhas++;
+            if(cicloAnt[i-1][j] == 'ORG')
+                vizinhas++;
+            if(cicloAnt[i-1][j+1] == 'ORG')
+                vizinhas++;
+            if(cicloAnt[i][j-1] == 'ORG')
+                vizinhas++;
+            if(cicloAnt[i][j+1] == 'ORG')
+                vizinhas++;
+            if(cicloAnt[i+1][j-1] == 'ORG')
+                vizinhas++;
+            if(cicloAnt[i+1][j] == 'ORG')
+                vizinhas++;
+            if(cicloAnt[i+1][j+1] == 'ORG')
+                vizinhas++;
+
+            if(cicloAnt[i][j] == 'VAZ' && vizinhas == 3)
+                cicloAtual[i][j] = 'ORG';
+            else if((cicloAnt[i][j] == 'ORG' && vizinhas < 2) || (cicloAnt[i][j] == 'ORG' && vizinhas > 3))
+                cicloAtual[i][j] = 'VAZ';
+
+            vizinhas = 0; //resetando contador
+        }
+
+        /*/Pra não escrever 8 comparações, também pensei em fazer as comparações do miolo assim:
+
+        for(x=-1; x<2; x++)
+        {
+            if(x == 0) //para não verificar cicloAnt[i][j]
+                for(y=-1; y<2; y+=2)
+                    if(cicloAnt[i+x][j+y] == 'ORG')
+                        vizinhas++;
+            else
+                for(y=-1; y<2; y++)
+                    if(cicloAnt[i+x][j+y] == 'ORG')
+                        vizinhas++;
+
+        }
+
+        Só que dessa forma ao invés de 8 comparações por posição, teríamos 11 por posição, oque para uma matriz muito grande poderia pesar talvez, n sei
+        /*/
+
+    }
+
+}
