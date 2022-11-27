149e11320d929ae279ee69ead22710600a172df5
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-11-27 11:51:05 -0300

    refatora padroes

--- src/jogo-da-vida.c
+++ src/jogo-da-vida.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <windows.h>
+#include "shared.c"
 #include "uteis.c"
+#include "padroes.c"
 
 #define TAM 101
 
@@ -66,12 +69,6 @@ void imprimeMatriz(char **matriz, int nl, int nc)
   }
 }
 
-void inicBlinker(char **m, int nL, int nC);
-void inicBloco(char **m, int nL, int nC);
-void inicSapo(char **m, int nL, int nC);
-void inicGlider(char **m, int nL, int nC);
-void inicLWSS(char **m, int nL, int nC);
-
 void menuInicJogo(char **mat, int nL, int nC)
 {
   int opcao;
@@ -103,71 +100,4 @@ void menuInicJogo(char **mat, int nL, int nC)
   while (getchar() != '\n')
     ;
   getchar();
-}
-
-void inicBlinker(char **m, int nL, int nC)
-{
-  char padrao[1][3] = {{ORG, ORG, ORG}};
-  int i, j, xInic = nL / 2, yInic = nC / 2;
-
-  limpaMatriz(m, nL, nC);
-
-  for (i = 0; i < 1; i++)
-    for (j = 0; j < 3; j++)
-      m[xInic + i][yInic + j] = padrao[i][j];
-}
-
-void inicBloco(char **m, int nL, int nC)
-{
-  char padrao[2][2] = {{ORG, ORG}, {ORG, ORG}};
-  int i, j, xInic = nL / 2, yInic = nC / 2;
-
-  limpaMatriz(m, nL, nC);
-
-  for (i = 0; i < 2; i++)
-    for (j = 0; j < 2; j++)
-      m[xInic + i][yInic + j] = padrao[i][j];
-}
-
-void inicSapo(char **m, int nL, int nC)
-{
-
-  char padrao[2][4] = {{VAZ, ORG, ORG, ORG}, {ORG, ORG, ORG, VAZ}};
-  int i, j, xInic = nL / 2, yInic = nC / 2;
-
-  limpaMatriz(m, nL, nC);
-
-  for (i = 0; i < 2; i++)
-    for (j = 0; j < 4; j++)
-      m[xInic + i][yInic + j] = padrao[i][j];
-}
-
-void inicGlider(char **m, int nL, int nC)
-{
-  char padrao[3][3] = {{ORG, ORG, ORG}, {ORG, VAZ, VAZ}, {VAZ, ORG, VAZ}};
-  int i, j, xInic, yInic;
-
-  limpaMatriz(m, nL, nC);
-
-  xInic = nL - 4;
-  yInic = nC - 4;
-
-  for (i = 0; i < 3; i++)
-    for (j = 0; j < 3; j++)
-      m[xInic + i][yInic + j] = padrao[i][j];
-}
-
-void inicLWSS(char **m, int nL, int nC)
-{
-  char padrao[4][5] = {{VAZ, ORG, VAZ, VAZ, ORG}, {ORG, VAZ, VAZ, VAZ, VAZ}, {ORG, VAZ, VAZ, VAZ, ORG}, {ORG, ORG, ORG, ORG, VAZ}};
-  int i, j, xInic, yInic;
-
-  limpaMatriz(m, nL, nC);
-
-  xInic = nL - 5;
-  yInic = nC - 6;
-
-  for (i = 0; i < 4; i++)
-    for (j = 0; j < 5; j++)
-      m[xInic + i][yInic + j] = padrao[i][j];
 }
