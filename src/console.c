2209b9352059e8a2728d24f8de17cf54471b7afe
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-11-30 22:24:43 -0300

    refatora funções do console para o arquivo console.c

---
+++ src/console.c
@@ -0,0 +1,75 @@
+#include <windows.h>
+
+#ifndef ENABLE_VIRTUAL_TERMINAL_PROCESSING
+#define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
+#endif
+
+#define MOSTRA_CURSOR "\e[?25l"
+#define ESCONDE_CURSOR "\e[?25h"
+
+typedef struct
+{
+  int width;
+  int height;
+} Terminal_Size;
+
+/**
+ * @brief Esconde/mostra o cursor no console. Adaptado de:
+ * https://www.autoscripts.net/c-hide-cursor/
+ *
+ * @param show Se verdadeiro, mostra, do contrário esconde.
+ */
+void toggleCursor(int show)
+{
+  assert(show == 0 || show == 1);
+
+#ifdef _WIN32
+  HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+  CONSOLE_CURSOR_INFO cci;
+  GetConsoleCursorInfo(handle, &cci);
+  cci.bVisible = show;
+  SetConsoleCursorInfo(handle, &cci);
+#elif __linux__
+  printf("%s", show ? MOSTRA_CURSOR : ESCONDE_CURSOR);
+#endif // Windows/Linux
+}
+
+/**
+ * Mede a largura e a altura (em número de caracteres disponíveis) do terminal rodando o programa.
+ * Adaptado de: // https://stackoverflow.com/a/12642749
+ *
+ * @param tsize Ponteiro para uma variável do tipo Terminal_Size
+ */
+void tamanhoTerminal(Terminal_Size *tsize)
+{
+  CONSOLE_SCREEN_BUFFER_INFO csbi;
+
+  GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+  tsize->width = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+  tsize->height = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+}
+
+/**
+ * @brief Configura o terminal para aceitar escapes. Fonte:
+ * https://solarianprogrammer.com/2019/04/08/c-programming-ansi-escape-codes-windows-macos-linux-terminals/
+ */
+void setupConsole()
+{
+  HANDLE stdoutHandle;
+  DWORD outModeInit;
+  DWORD outMode = 0;
+  stdoutHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+  if (stdoutHandle == INVALID_HANDLE_VALUE)
+    exit(GetLastError());
+
+  if (!GetConsoleMode(stdoutHandle, &outMode))
+    exit(GetLastError());
+
+  outModeInit = outMode;
+
+  outMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+
+  if (!SetConsoleMode(stdoutHandle, outMode))
+    exit(GetLastError());
+}
--- src/interface.c
+++ src/interface.c
@@ -2,21 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <windows.h>
 
 #include "uteis.c"
-
-#ifndef ENABLE_VIRTUAL_TERMINAL_PROCESSING
-#define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
-#endif
+#include "console.c"
 
 #define CHAR_CELULA_MORTA '.'
 #define CHAR_CELULA_VIVA 'O'
 
 #define APAGA_LINHA "\x1b[2K"
 #define COMECO_LINHA_ANT "\x1b[1F"
-#define MOSTRA_CURSOR "\e[?25l"
-#define ESCONDE_CURSOR "\e[?25h"
+
 #define RESET "\033[0m"
 #define COR_AZUL "\033[34m"
 #define COR_VERDE "\033[92m"
@@ -24,12 +19,6 @@
 #define COR_AMARELO "\033[93m"
 #define COR_VERMELHO "\033[31m"
 #define COR_PADRAO COR_VERDE
-
-typedef struct
-{
-  int width;
-  int height;
-} Terminal_Size;
 
 typedef enum
 {
@@ -73,67 +62,6 @@ void imprimeMatriz(char **matriz, int nl, int nc)
 
     printf("\n");
   }
-}
-
-/**
- * @brief Esconde/mostra o cursor no console. Adaptado de:
- * https://www.autoscripts.net/c-hide-cursor/
- *
- * @param show Se verdadeiro, mostra, do contrário esconde.
- */
-void toggleCursor(int show)
-{
-  assert(show == 0 || show == 1);
-
-#ifdef _WIN32
-  HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
-  CONSOLE_CURSOR_INFO cci;
-  GetConsoleCursorInfo(handle, &cci);
-  cci.bVisible = show;
-  SetConsoleCursorInfo(handle, &cci);
-#elif __linux__
-  printf("%s", show ? MOSTRA_CURSOR : ESCONDE_CURSOR);
-#endif // Windows/Linux
-}
-
-/**
- * Mede a largura e a altura (em número de caracteres disponíveis) do terminal rodando o programa
- *
- * @param tsize Ponteiro para uma variável do tipo Terminal_Size
- */
-void tamanhoTerminal(Terminal_Size *tsize)
-{
-  CONSOLE_SCREEN_BUFFER_INFO csbi;
-
-  // https://stackoverflow.com/a/12642749
-  GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-  tsize->width = csbi.srWindow.Right - csbi.srWindow.Left + 1;
-  tsize->height = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
-}
-
-/**
- * @brief Configura o terminal para aceitar escapes. Fonte:
- * https://solarianprogrammer.com/2019/04/08/c-programming-ansi-escape-codes-windows-macos-linux-terminals/
- */
-void setupConsole()
-{
-  HANDLE stdoutHandle;
-  DWORD outModeInit;
-  DWORD outMode = 0;
-  stdoutHandle = GetStdHandle(STD_OUTPUT_HANDLE);
-
-  if (stdoutHandle == INVALID_HANDLE_VALUE)
-    exit(GetLastError());
-
-  if (!GetConsoleMode(stdoutHandle, &outMode))
-    exit(GetLastError());
-
-  outModeInit = outMode;
-
-  outMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
-
-  if (!SetConsoleMode(stdoutHandle, outMode))
-    exit(GetLastError());
 }
 
 int contStr(const char *firstArg, va_list args);
